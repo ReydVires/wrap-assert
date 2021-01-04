fd6960f916a1b18739ef56186e55271864119817
Ahmad Arsyel <arsyel@agate.id>
2021-01-04 12:09:08 +0700

    change to function

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+module.exports = assert;
+
 /**
  * Wrapping the assert log with some description
  * @param {boolean} value Evaluation parameter
  * @param {string} [label] Test label
  * @param {string} [failMessage] Log the message when the test is fail
  */
-module.exports = (value, label, failMessage) => {
+function assert (value, label, failMessage) {
 	const assertLabel = (label) ? `${label} test` : "Test";
 	const isPass = (value) ? "pass ✅" : "fail ❌";
 	const message = `${assertLabel} is ${isPass}`;
